875cccffb4a2c00eea3eaa08c95c3c2ab4a5dbe4
Joshuaijaola1 <70036783+lingeringwill@users.noreply.github.com>
2022-02-23 16:41:18 +0000

    Initial commit

---
+++ Room.java
@@ -0,0 +1,10 @@
+class Room {
+  private int room_num;
+  //private Boss 
+
+  Room(int room) {// initializes room number, I will have to enter in the room number manually
+    this.room_num = room; 
+  }
+
+  
+}
